0733f5fd9bdf81805e8334c40d93cae0999341dc
Jatin <jatin@illiquidlabs.com>
2023-10-28 19:42:07 +0530

    Updated Project to C++

---
+++ Source/chakra_clash/Public/EOSGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EOSGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAKRA_CLASH_API UEOSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/chakra_clash/chakra_clash.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "chakra_clash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, chakra_clash, "chakra_clash" );
